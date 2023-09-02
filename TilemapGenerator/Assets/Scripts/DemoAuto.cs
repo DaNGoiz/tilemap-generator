0d83524cbb417015c46605e442cf8b235cc7014f
Izzy <3332099165@qq.com>
2023-09-02 19:18:04 +0800

    Basic Generate Functions Done

--- TilemapGenerator/Assets/Scripts/DemoAuto.cs
+++ TilemapGenerator/Assets/Scripts/DemoAuto.cs
@@ -1,39 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class DemoAuto : MonoBehaviour
 {
-    public Tile[] groundTiles;
-    private Tilemap groundTilemap;
+    // get all current level tiles (each level of tiles are named by natural number, i.e. start from 0)
+    // get the tilemap of lower layer
+    // generate the current layer with rules given
 
-    public Tilemap waterTilemap; // temp public, should have better solution
-    private List<Vector3Int> waterTileWorldPos = new List<Vector3Int>();
+    // - cannot generate out of the lower layer area, unless special position area is defined
+
+    public TextAsset tileRuleTextFile;
+    private Dictionary<int, Dictionary<string, Dictionary<int, int>>> ruleDictionary;
     
+    public Tile[] currentLayerTiles;
+    private Tilemap currentLayerTilemap;
+
+    public Tilemap lowLayerTilemap; // - temp public, should have better solution
+    private List<Vector3Int> lowerLayerTileWorldPos = new List<Vector3Int>();
+    
+    private Dictionary<Vector3Int, int> existingTiles = new Dictionary<Vector3Int, int>();
+
     void Start()
     {
-        groundTilemap = GetComponent<Tilemap>();
-        waterTilemap = GetComponent<Tilemap>();
+        currentLayerTilemap = GetComponent<Tilemap>();
+        lowLayerTilemap = GetComponent<Tilemap>();
 
-        GetAllWaterTilePos();
+        GetAllLowerLayerTilePos();
         
+        ruleDictionary = ReadRule.LoadDictionaryFromTextAsset(tileRuleTextFile);
 
-
-        //test
-        // groundTilemap.SetTile(new Vector3Int(0,0,0),groundTiles[0]);
+        // test
+        // existingTiles.Add(new Vector3Int(1,0,0), 0);
+        // existingTiles.Add(new Vector3Int(0,-1,0), 0);
+        // print(ChooseOneTile(new Vector3Int(0,0,0)));
+        // currentLayerTilemap.SetTile(new Vector3Int(0,0,0),currentLayerTiles[ChooseOneTile(new Vector3Int(0,0,0))]);
+        
+        // debug
+        // foreach (var tile in ruleDictionary)
+        // {
+        //     Debug.Log("Key: " + tile.Key);
+        //     foreach (var direction in tile.Value)
+        //     {
+        //         Debug.Log("Direction: " + direction.Key);
+        //         foreach (var tileNo in direction.Value)
+        //         {
+        //             Debug.Log("TileNo: " + tileNo.Key + ", Weight: " + tileNo.Value);
+        //         }
+        //     }
+        // }
     }
 
-    private void GetAllWaterTilePos()
+    private void GetAllLowerLayerTilePos()
     {
         //get all positions of water tiles
-        Vector3Int waterOrg = waterTilemap.origin;
-        Vector3Int waterSize = waterTilemap.size;
-        for(int x = waterOrg.x; x < waterSize.x + waterOrg.x; x++)
+        Vector3Int lowerLayerOrg = lowLayerTilemap.origin;
+        Vector3Int lowerLayerSize = lowLayerTilemap.size;
+        // print(lowerLayerOrg + " " + lowerLayerSize);
+        for(int x = lowerLayerOrg.x; x < lowerLayerSize.x + lowerLayerOrg.x; x++)
         {
-            for(int y = waterOrg.y; y < waterSize.y + waterOrg.y; y++)
+            for(int y = lowerLayerOrg.y; y < lowerLayerSize.y + lowerLayerOrg.y; y++)
             {
-                waterTileWorldPos.Add(new Vector3Int(x,y,0));
+                lowerLayerTileWorldPos.Add(new Vector3Int(x,y,0));
             }
         }
     }
@@ -45,10 +76,140 @@ public class DemoAuto : MonoBehaviour
 
     private IEnumerator DemoAutoMap()
     {
-        foreach(Vector3Int v in waterTileWorldPos)
+        if(!existingTiles.ContainsKey(lowerLayerTileWorldPos[0])){existingTiles.Add(lowerLayerTileWorldPos[0], 46);}
+        foreach(Vector3Int vec in lowerLayerTileWorldPos)
         {   
-            groundTilemap.SetTile(v,groundTiles[0]);
-            yield return new WaitForSeconds(0.3f);
+            if(vec == lowerLayerTileWorldPos[0]){continue;}
+            int tile = ChooseOneTile(vec);
+            if(tile == -1){continue;}
+            if(!existingTiles.ContainsKey(vec))
+            {
+                currentLayerTilemap.SetTile(vec,currentLayerTiles[tile]);
+                if(!existingTiles.ContainsKey(vec)){existingTiles.Add(vec, tile);}
+                else{existingTiles[vec] = tile;}
+            }
+            
+            yield return new WaitForSeconds(0.01f);
         } 
+    }
+
+    private int ChooseOneTile(Vector3Int currentTilePos)
+    {
+        Dictionary<int, int> tileNumberAndWeight_up = new Dictionary<int, int>();
+        Dictionary<int, int> tileNumberAndWeight_down = new Dictionary<int, int>();
+        Dictionary<int, int> tileNumberAndWeight_left = new Dictionary<int, int>();
+        Dictionary<int, int> tileNumberAndWeight_right = new Dictionary<int, int>();
+
+        tileNumberAndWeight_up = GetAvailableTileNumberAndWeightFromSurround(new Vector3Int(currentTilePos.x, currentTilePos.y + 1, 0), "down");
+        tileNumberAndWeight_down = GetAvailableTileNumberAndWeightFromSurround(new Vector3Int(currentTilePos.x, currentTilePos.y - 1, 0), "up");
+        tileNumberAndWeight_left = GetAvailableTileNumberAndWeightFromSurround(new Vector3Int(currentTilePos.x - 1, currentTilePos.y, 0), "right");
+        tileNumberAndWeight_right = GetAvailableTileNumberAndWeightFromSurround(new Vector3Int(currentTilePos.x + 1, currentTilePos.y, 0), "left");
+
+        // combine the weights together
+        // (intersection, if have same tile with different weight, choose the higher one)
+        // if unavailable, debug and return a special tile number for emptiness
+
+        List<Dictionary<int, int>> dictionaries = new List<Dictionary<int, int>>
+        {
+            tileNumberAndWeight_up,
+            tileNumberAndWeight_down,
+            tileNumberAndWeight_left,
+            tileNumberAndWeight_right
+        };
+
+        dictionaries = dictionaries.Where(dict => dict != null).ToList();
+        var commonKeys = dictionaries
+            .SelectMany(dict => dict.Keys)
+            .GroupBy(key => key)
+            .Where(group => group.Count() == dictionaries.Count)
+            .Select(group => group.Key);
+
+        // if (commonKeys.Count() == 0)
+        // {
+        //     Debug.Log("No common key");
+        // }
+
+        Dictionary<int, int> commonTileNumberAndWeight = new Dictionary<int, int>();
+        foreach (var dict in dictionaries)
+        {
+            foreach (int key in commonKeys)
+            {
+                if (dict.ContainsKey(key))
+                {
+                    if (!commonTileNumberAndWeight.ContainsKey(key))
+                    {
+                        commonTileNumberAndWeight.Add(key, dict[key]);
+                    }
+                    else
+                    {
+                        if (commonTileNumberAndWeight[key] < dict[key])
+                        {
+                            commonTileNumberAndWeight[key] = dict[key];
+                        }
+                    }
+                }
+            }
+        }
+
+        // foreach (var tileNo in commonTileNumberAndWeight)
+        // {
+        //     Debug.Log("TileNo: " + tileNo.Key + ", Weight: " + tileNo.Value);
+        // }
+
+        
+        // generate a random number from total weight and refer it to a tile
+        int totalWeight = 0;
+        foreach (int weight in commonTileNumberAndWeight.Values)
+        {
+            totalWeight += weight;
+        }
+        int randomNumber = Random.Range(1, totalWeight + 1);
+        int cumulativeWeight = 0;
+        int keyTileNumber = -1;
+        foreach (var kvp in commonTileNumberAndWeight)
+        {
+            cumulativeWeight += kvp.Value;
+            if (randomNumber <= cumulativeWeight)
+            {
+                keyTileNumber = kvp.Key;
+                // return the tile number i, currentLayerTiles[i] will be the next tile generated
+                return keyTileNumber;
+            }
+        }
+        return keyTileNumber;
+    }
+
+    private Dictionary<int, int> GetAvailableTileNumberAndWeightFromSurround(Vector3Int surroundTilePos, string targetDirection)
+    {
+        if(!lowerLayerTileWorldPos.Contains(surroundTilePos)){return null;}
+        if(existingTiles.ContainsKey(surroundTilePos))
+        {
+            Dictionary<int, int> tileNumberAndWeight = new Dictionary<int, int>();
+            int surroundTileNo = existingTiles[surroundTilePos];
+            if(ruleDictionary[surroundTileNo].ContainsKey(targetDirection))
+            {
+                foreach (var tileNo in ruleDictionary[surroundTileNo][targetDirection])
+                {
+                    if (!tileNumberAndWeight.ContainsKey(tileNo.Key))
+                    {
+                        tileNumberAndWeight.Add(tileNo.Key, tileNo.Value);
+                    }
+                    else
+                    {
+                        if (tileNumberAndWeight[tileNo.Key] < tileNo.Value)
+                        {
+                            tileNumberAndWeight[tileNo.Key] = tileNo.Value;
+                        }
+                    }
+                }
+                return tileNumberAndWeight;
+            }
+            else{return null;}
+        }
+        else
+        {
+            // return new Dictionary<int, int>{{ -1, -1 }};
+            return null;
+        }
     }
 }
---
+++ TilemapGenerator/Assets/Scripts/ReadRule.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadRule : MonoBehaviour
+{
+    // Given a test asset, return a rule dictionary
+    // structure: currentTileNo -> direction -> canConstructTileNo -> weight
+    public static Dictionary<int, Dictionary<string, Dictionary<int, int>>> LoadDictionaryFromTextAsset(TextAsset tileRuleTextFile)
+    {
+        Dictionary<int, Dictionary<string, Dictionary<int, int>>> ruleDictionary = new Dictionary<int, Dictionary<string, Dictionary<int, int>>>();
+        
+        if (tileRuleTextFile != null)
+        {
+            string fileContent = tileRuleTextFile.text;
+            string[] lines = fileContent.Split('\n');
+
+            int currentTileNo = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i] = lines[i].Trim();
+                if (lines[i].StartsWith("["))
+                {
+                    currentTileNo = int.Parse(lines[i].Substring(1, lines[i].Length - 2)); // maybe need to -1
+                    ruleDictionary.Add(currentTileNo, new Dictionary<string, Dictionary<int, int>>());
+                }
+                else if (lines[i].StartsWith("up") || lines[i].StartsWith("down") || lines[i].StartsWith("left") || lines[i].StartsWith("right"))
+                {
+                    string[] directoinTiles = lines[i].Split(':');
+                    string[] tiles = directoinTiles[1].Split(',');
+                    for (int j = 0; j < tiles.Length; j++)
+                    {
+                        tiles[j] = tiles[j].Trim();
+                        string[] tileNoAndWeight = tiles[j].Split('*');
+                        int tileNo = int.Parse(tileNoAndWeight[0]);
+                        int weight = int.Parse(tileNoAndWeight[1]);
+                        
+                        if (!ruleDictionary[currentTileNo].ContainsKey(directoinTiles[0])){ruleDictionary[currentTileNo].Add(directoinTiles[0], new Dictionary<int, int>());}
+                        if (!ruleDictionary[currentTileNo][directoinTiles[0]].ContainsKey(tileNo)){ruleDictionary[currentTileNo][directoinTiles[0]].Add(tileNo, weight);}
+                    }
+                }
+            }
+        }
+
+        return ruleDictionary;
+    }
+}
